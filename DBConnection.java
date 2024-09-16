188c6d2680c7c894d94f8299a400a96a87e61872
Mathumitharajendran <139482884+Mathumitharajendran@users.noreply.github.com>
2024-09-16 11:23:19 +0530

    DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,23 @@
+package RideRail;
+
+
+	
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+	public class DBConnection {
+	    public static Connection getConnection() {
+	        Connection con = null;
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/RideRail", "root", "password");
+	        } catch (ClassNotFoundException | SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return con;
+	    }
+	}
+
+
+
